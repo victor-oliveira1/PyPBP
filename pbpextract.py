a70109574230a37b5d437f08c0e55982ed999513
Victor Oliveira <victor.oliveira@gmx.com>
2018-04-16 08:18:32 -0300

    Add files via upload

---
+++ pbpextract.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+#victor.oliveira@gmx.com
+import struct
+import zlib
+
+class PBPExtract:
+    def __init__(self):
+        self.pbp_name = 'RE.PBP'
+        self.img_name = 'IMAGE.IMG'
+        self.pbp_file = open(self.pbp_name, 'rb')
+        self.img_file = open(self.img_name, 'wb')
+
+        #Getting the psar offset
+        self.pbp_file.seek(36)
+        self.psar_offset = struct.unpack('i', self.pbp_file.read(4))[0]
+
+    def _isMultiDisk(self):
+        self.pbp_file.seek(self.psar_offset)
+        tmp = self.pbp_file.read(10)
+        if b'PSTITLEIMG' in tmp:
+            return True
+        elif b'PSISOIMG' in tmp:
+            return False
+        else:
+            raise ValueError('The PBP file is damaged')
+
+    def _getImageIndexes(self, multidisk=False):
+        if multidisk:
+            pbp_file.seek(self.psar_offset + 512)
+            self.iso_psar_offset = struct.unpack('iiii', pbp_file.read(16))
+            for psar_offset in iso_psar_offset:
+                self.pbp_file.seek(self.psar_offset + 16384)
+                indexes = dict()
+                for i in range(0, 32256):
+                    offset, size = struct.unpack('ii', self.pbp_file.read(8))
+                    self.pbp_file.read(24)
+                    if offset or size:
+                        indexes.update({offset : size})
+                return indexes
+            
+        self.pbp_file.seek(self.psar_offset + 16384)
+        indexes = dict()
+        for i in range(0, 32256):
+            offset, size = struct.unpack('ii', self.pbp_file.read(8))
+            self.pbp_file.read(24)
+            if offset or size:
+                indexes.update({offset : size})
+        return indexes
+
+    def _writeBlocks(self, psar_offset, indexes):
+        for index in indexes.items():
+            offset = index[0]
+            size = index[1]
+            self.pbp_file.seek(psar_offset + 1048576 + offset)
+            buffer = self.pbp_file.read(size)
+            if size is not 37632:
+                buffer = zlib.decompress(buffer, -15)
+            self.img_file.write(buffer)
